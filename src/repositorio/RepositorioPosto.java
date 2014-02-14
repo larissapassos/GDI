c884c8b2a6953653dad8e5e0c52fa8310a240d23
bmca <bmca@cin.ufpe.br>
2014-02-14 16:27:37 -0300

    remover e inserir

--- src/repositorio/RepositorioPosto.java
+++ src/repositorio/RepositorioPosto.java
@@ -1,7 +1,12 @@
 package repositorio;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import objetos.Posto;
@@ -16,7 +21,27 @@ public class RepositorioPosto extends Repositorio {
 	@Override
 	public void inserir(Object objeto) {
 		// TODO Auto-generated method stub
+		try{
+			Posto posto = (Posto)objeto;
+			PreparedStatement inserirPosto = con.prepareStatement("INSERT INTO tb_posto('?', '?', (SELECT REF(e) FROM tb_endereco e WHERE e.cep = '?'), ?)");
+			
+			inserirPosto.setString(1, posto.codigo);
+			inserirPosto.setString(2, posto.nome);
+			inserirPosto.setString(3, posto.endereco.cep);
+			
+			File blob = new File(posto.imagePath);
+			
+			FileInputStream in = new FileInputStream(blob);
+			
+			inserirPosto.setBinaryStream(4, in,(int)blob.length());
+			inserirPosto.execute();
 		
+		}catch (SQLException exex) { 
+			exex.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -28,7 +53,26 @@ public class RepositorioPosto extends Repositorio {
 	@Override
 	public boolean remover(Object objeto) {
 		// TODO Auto-generated method stub
-		return false;
+		boolean removeu = false;
+		String codigo = ((Posto)objeto).codigo;
+		String delete = " DELETE FROM tb_posto WHERE codigo=" + codigo;
+		Statement stmt;
+		try{
+			stmt = con.createStatement();
+			stmt.executeUpdate(delete);
+			System.out.println(" Delecao realizada ");
+			removeu = true;
+			stmt.close();
+		} catch(SQLException ex2){
+			ex2.printStackTrace();
+			try {
+				con.rollback();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return removeu;
 	}
 
 	@Override
